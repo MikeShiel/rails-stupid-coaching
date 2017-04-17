a6cf71e1024bf67cb30a742ee3e9edcd55133763
Mike Shiel <shiel90@gmail.com>
2017-04-17 13:28:56 +0200

    changes added

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,20 @@
+class CoachingController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @query = params[:query]
+    @coach_answer = coach_answer(@query)
+  end
+
+
+  def coach_answer(your_message)
+    if your_message == "I am going to work right now!"
+      return ""
+    elsif your_message.include? "?"
+      return "Silly question, get dressed and go to work!"
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
